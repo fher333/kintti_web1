870645a619a26314044a7b6b5581386f95a1a178
fernando melendez <fher.melendez@gmail.com>
2021-06-04 12:02:22 -0500

    deploy2a

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -24,6 +24,7 @@ const productSchema = new mongoose.Schema(
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
     reviews: [reviewSchema],
+    phone: { type: String, required: true },
     
   },
   {
